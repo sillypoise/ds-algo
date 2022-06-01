2a3c3a7b2da44f5c465a56803ce00a081df34a07
sillypoise <somewhatjose@gmail.com>
2022-06-01 14:34:16 -0500

    Added valid palindrom algo in TypeScript with one solution

---
+++ Problems/01. Easy/Valid Palindrome/TypeScript/ValidPalilndrome.ts
@@ -0,0 +1,23 @@
+function ValidPalindrome(s: string): boolean {
+    return true;
+}
+
+function S1_ValidPalindrome(s: string): boolean {
+    // ⌚💾 O(n)
+    let normS = normaliseString(s);
+    if (!normS.length) return true;
+    let leftP = 0;
+    let rightP = normS.length - 1;
+    // ⌚ O(n)
+    while (leftP <= rightP) {
+        if (normS[leftP] !== normS[rightP]) return false;
+        leftP++;
+        rightP--;
+    }
+    return true;
+}
+
+//  HELPERS
+function normaliseString(s: string): string {
+    return s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+}
