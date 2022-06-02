99a4abbefcc6322efd1e176aca265cf0bc46ff32
sillypoise <somewhatjose@gmail.com>
2022-06-02 12:36:12 -0500

    Added best time to buy and sell stock / max profit algo in TypeScript

--- Problems/01. Easy/Best Time to Buy and Sell Stock/TypeScript/BestTimeToBuyAndSellStock.ts
+++ Problems/01. Easy/Best Time to Buy and Sell Stock/TypeScript/BestTimeToBuyAndSellStock.ts
@@ -1,11 +1,16 @@
 function BestTimeToBuyAndSellStock(prices: number[]): number {
     if (prices.length <= 1) return 0;
 
+    return 0;
+}
+
+function S1_BestTimeToBuyAndSellStock(prices: number[]): number {
+    if (prices.length <= 1) return 0;
     let leftP = 0;
     let rightP = leftP + 1;
     let maxProfit = 0;
     // ⌚ O(n)
-    while (rightP < prices.length - 1) {
+    while (rightP < prices.length) {
         if (prices[leftP] > prices[rightP]) {
             leftP = rightP;
         } else {
@@ -16,5 +21,3 @@ function BestTimeToBuyAndSellStock(prices: number[]): number {
     }
     return maxProfit;
 }
-
-BestTimeToBuyAndSellStock([7, 1, 5, 3, 6, 4]);
