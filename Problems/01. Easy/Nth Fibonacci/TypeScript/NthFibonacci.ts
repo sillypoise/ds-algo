166355eb735ae0eb1a3c53b7e40a6d27db59a51e
sillypoise <somewhatjose@gmail.com>
2022-06-27 13:39:16 -0500

    Nth Fibonacci solutions in TypeScript

---
+++ Problems/01. Easy/Nth Fibonacci/TypeScript/NthFibonacci.ts
@@ -0,0 +1,40 @@
+function NthFib(n: number): number {
+    return 0;
+}
+
+function S1_NthFib(n: number): number {
+    if (n === 2) {
+        return 1;
+    } else if (n === 1) {
+        return 0;
+    } else {
+        return S1_NthFib(n - 1) + S1_NthFib(n - 2);
+    }
+}
+
+function S2_NthFib(
+    n: number,
+    memo: Record<number, number> = { 1: 0, 2: 0 }
+): number {
+    if (n in memo) {
+        return memo[n];
+    } else {
+        memo[n] = S2_NthFib(n - 1, memo) + S2_NthFib(n - 2, memo);
+        return memo[n];
+    }
+}
+
+function S3_NthFib(n: number): number {
+    let lastTwo = [0, 1];
+    let counter = 3;
+
+    while (counter <= n) {
+        const nextFib = lastTwo[0] + lastTwo[1];
+        lastTwo[0] = lastTwo[1];
+        lastTwo[1] = nextFib;
+        counter++;
+    }
+    return n > 1 ? lastTwo[1] : lastTwo[0];
+}
+
+S3_NthFib(5);
--- src/debugger.ts
+++ src/debugger.ts
@@ -1,86 +1,14 @@
-interface TreeNode {
-    val: number;
-    left: TreeNode | null;
-    right: TreeNode | null;
-}
+function S3_NthFib(n: number): number {
+    let lastTwo = [0, 1];
+    let counter = 3;
 
-class TreeNode {
-    constructor(
-        public val: number,
-        public left: TreeNode | null,
-        public right: TreeNode | null
-    ) {
-        this.val = !val ? 0 : val;
-        this.left = !left ? null : left;
-        this.right = !right ? null : right;
+    while (counter <= n) {
+        const nextFib = lastTwo[0] + lastTwo[1];
+        lastTwo[0] = lastTwo[1];
+        lastTwo[1] = nextFib;
+        counter++;
     }
+    return n > 1 ? lastTwo[1] : lastTwo[0];
 }
 
-function S1_BranchSums(root: TreeNode | null): number[] {
-    const sums: number[] = [];
-
-    function dfs(
-        node: TreeNode | null,
-        pathSum: number,
-        sums: number[]
-    ): number[] | void {
-        if (!node) return;
-
-        let newPathSum = pathSum + node.val;
-
-        if (!node.left && !node.right) {
-            sums.push(newPathSum);
-            return;
-        }
-
-        dfs(node.left, newPathSum, sums);
-        dfs(node.right, newPathSum, sums);
-    }
-
-    dfs(root, 0, sums);
-    return sums;
-}
-
-let t: TreeNode = {
-    val: 1,
-    left: {
-        val: 2,
-        left: {
-            val: 4,
-            left: {
-                val: 8,
-                left: null,
-                right: null,
-            },
-            right: {
-                val: 9,
-                left: null,
-                right: null,
-            },
-        },
-        right: {
-            val: 5,
-            left: {
-                val: 10,
-                left: null,
-                right: null,
-            },
-            right: null,
-        },
-    },
-    right: {
-        val: 3,
-        left: {
-            val: 6,
-            left: null,
-            right: null,
-        },
-        right: {
-            val: 7,
-            left: null,
-            right: null,
-        },
-    },
-};
-
-S1_BranchSums(t);
+S3_NthFib(5);
