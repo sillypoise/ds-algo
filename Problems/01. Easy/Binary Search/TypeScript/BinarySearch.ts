e66a5139ab363b620b132c205131880d019a459b
sillypoise <somewhatjose@gmail.com>
2022-06-27 17:37:28 -0500

    Added second solution to binary search in TS

--- Problems/01. Easy/Binary Search/TypeScript/BinarySearch.ts
+++ Problems/01. Easy/Binary Search/TypeScript/BinarySearch.ts
@@ -17,3 +17,24 @@ function S1_BinarySearch(nums: number[], target: number): number {
     }
     return -1;
 }
+
+function S2_BinarySearch(nums: number[], target: number): number {
+    function binarySearchHelper(
+        nums: number[],
+        target: number,
+        left: number = 0,
+        right: number = nums.length - 1
+    ) {
+        if (left > right) return -1;
+        let pivot = Math.floor((left + right) / 2);
+
+        if (nums[pivot] === target) return pivot;
+        if (nums[pivot] > target) {
+            return binarySearchHelper(nums, target, left, pivot - 1);
+        } else {
+            return binarySearchHelper(nums, target, pivot + 1, right);
+        }
+    }
+
+    return binarySearchHelper(nums, target, 0, nums.length - 1);
+}
