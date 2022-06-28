eaab34067bff9a573d48e10844e75de3cf775fdb
sillypoise <somewhatjose@gmail.com>
2022-06-28 15:56:04 -0500

    Added Run-Length Encoding solution in TypeScript

---
+++ Problems/01. Easy/Run-Length Encoding/TypeScript/RunLengthEncoding.ts
@@ -0,0 +1,25 @@
+function RunLengthEncoding(input: string): string {
+    return "";
+}
+
+function S1_RunLengthEncoding(input: string): string {
+    let encodedChars: string[] = [];
+    let currLength = 1;
+
+    for (let i = 1; i < input.length; i++) {
+        let currChar = input[i];
+        let prevChar = input[i - 1];
+
+        if (currChar !== prevChar || currLength == 9) {
+            encodedChars.push(currLength.toString());
+            encodedChars.push(prevChar);
+            currLength = 0;
+        }
+
+        currLength++;
+    }
+    encodedChars.push(currLength.toString());
+    encodedChars.push(input[input.length - 1]);
+
+    return encodedChars.join("");
+}
