8df21e0496bd9706740872c2b49cfa840231dd1a
Silvio <somewhatjose@gmail.com>
2022-07-19 19:07:50 -0500

    Added solutions for Find the Duplicate Number in TypeScript

--- Problems/02. Medium/Find the Duplicate Number/TypeScript/FindDuplicateNumber.ts
+++ Problems/02. Medium/Find the Duplicate Number/TypeScript/FindDuplicateNumber.ts
@@ -1,3 +1,26 @@
-function FindDuplicateNumber() {
-    return;
+function FindDuplicateNumber(nums: Array<number>): number {
+    return 0;
 }
+
+function S1_FindDuplicateNumber(nums: Array<number>): number {
+    let slowP = 0;
+    let fastP = 0;
+
+    while (true) {
+        slowP = nums[slowP];
+        fastP = nums[nums[fastP]];
+
+        if (slowP === fastP) {
+            break;
+        }
+    }
+    let slowP2 = 0;
+    while (true) {
+        slowP = nums[slowP];
+        slowP2 = nums[slowP2];
+        if (slowP === slowP2) return slowP;
+    }
+}
+
+S1_FindDuplicateNumber([1, 3, 4, 2, 2]);
+S1_FindDuplicateNumber([3, 1, 3, 4, 2]);
--- src/debugger.ts
+++ src/debugger.ts
@@ -1,54 +1,22 @@
-interface ListNode {
-    val: number;
-    next: ListNode | null;
-}
+function S1_FindDuplicateNumber(nums: Array<number>): number {
+    let slowP = 0;
+    let fastP = 0;
 
-class ListNode {
-    val: number;
-    next: ListNode | null;
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = !val ? 0 : val;
-        this.next = !next ? null : next;
+    while (true) {
+        slowP = nums[slowP];
+        fastP = nums[nums[fastP]];
+
+        if (slowP === fastP) {
+            break;
+        }
+    }
+    let slowP2 = 0;
+    while (true) {
+        slowP = nums[slowP];
+        slowP2 = nums[slowP2];
+        if (slowP === slowP2) return slowP;
     }
 }
 
-function S1_RemoveNthFromEnd(
-    head: ListNode | null,
-    n: number
-): ListNode | null {
-    let dummy = new ListNode(0, head);
-    let left: ListNode | null = dummy;
-    let right = head;
-
-    while (n > 0 && right) {
-        right = right.next;
-        n--;
-    }
-
-    while (right) {
-        left = left && left.next;
-        right = right.next;
-    }
-
-    if (left) left.next = left.next && left.next.next;
-    return dummy.next;
-}
-
-let t = {
-    val: 1,
-    next: {
-        val: 2,
-        next: {
-            val: 3,
-            next: {
-                val: 4,
-                next: {
-                    val: 5,
-                    next: null,
-                },
-            },
-        },
-    },
-};
-
-S1_RemoveNthFromEnd(t, 2);
+S1_FindDuplicateNumber([1, 3, 4, 2, 2]);
+S1_FindDuplicateNumber([3, 1, 3, 4, 2]);
