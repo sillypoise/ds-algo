55857fd929c3049aa6e409b4d6348cc024c7b34e
sillypoise <somewhatjose@gmail.com>
2023-03-22 11:02:44 -0500

    Add eslint and prettier formatting

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ A personal repo used to store answers to different data-structures and algorithm
 I try to revisit problems I have found solutions for, add new problems and add solutions in different langauges. It serves as a place to not only store solutions but problems themselves.
 
 As of now it's mostly TypeScript solutions. But slowly try to add new languages.
-
-
--- src/debugger.ts
+++ src/debugger.ts
@@ -1,22 +1,41 @@
-function S1_FindDuplicateNumber(nums: Array<number>): number {
-    let slowP = 0;
-    let fastP = 0;
+// function S1_FindDuplicateNumber(nums: Array<number>): number {
+//     let slowP = 0;
+//     let fastP = 0;
+//
+//     while (true) {
+//         slowP = nums[slowP];
+//         fastP = nums[nums[fastP]];
+//
+//         if (slowP === fastP) {
+//             break;
+//         }
+//     }
+//     let slowP2 = 0;
+//     while (true) {
+//         slowP = nums[slowP];
+//         slowP2 = nums[slowP2];
+//         if (slowP === slowP2) return slowP;
+//     }
+// }
+//
+// S1_FindDuplicateNumber([1, 3, 4, 2, 2]);
+// S1_FindDuplicateNumber([3, 1, 3, 4, 2]);
 
-    while (true) {
-        slowP = nums[slowP];
-        fastP = nums[nums[fastP]];
+function diagonalDifference(arr: number[][]): number {
+    let matix_size = arr.length;
+    let primary_sum = 0;
+    let secondary_sum = 0;
 
-        if (slowP === fastP) {
-            break;
-        }
+    for (let i = 0; i < matix_size; ++i) {
+        primary_sum += arr[i][i];
+        secondary_sum += arr[i][matix_size - i - 1];
     }
-    let slowP2 = 0;
-    while (true) {
-        slowP = nums[slowP];
-        slowP2 = nums[slowP2];
-        if (slowP === slowP2) return slowP;
-    }
+
+    return Math.abs(primary_sum - secondary_sum);
 }
 
-S1_FindDuplicateNumber([1, 3, 4, 2, 2]);
-S1_FindDuplicateNumber([3, 1, 3, 4, 2]);
+diagonalDifference([
+    [1, 2, 3],
+    [4, 5, 6],
+    [9, 8, 9],
+]);
